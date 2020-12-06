9c6e17d8dd65713ba81e6359d7a3bd94d1925212
rupeshreddy24 <74659109+rupeshreddy24@users.noreply.github.com>
2020-12-06 12:26:17 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,34 @@
+class box
+{
+    constructor(x,y,width,height,angle)
+    {
+        var options={
+            isStatic: true 
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        Matter.Body.setAngle(this.body,angle);
+        World.add(world,this.body);
+    }
+
+    display()
+    {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+       
+        translate(pos.x,pos.y);
+        angleMode(RADIANS);
+        rotate(angle);
+        rectMode(CENTER);
+        strokeWeight(4);
+        fill("red");
+        
+
+        rect(0,0,this.width,this.height);
+
+        pop();
+    }
+}
